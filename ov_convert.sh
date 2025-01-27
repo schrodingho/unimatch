cb693c82d4ce01578f119bff6f8fe54121e32a43
schrodingho <dinghao.xue@intel.com>
2025-01-27 10:41:05 +0800

    add openvino conversion for unimatch model

---
+++ ov_convert.sh
@@ -0,0 +1,11 @@
+python main_flow_ov.py --inference_dir demo/flow-davis \
+--resume pretrained/gmflow-scale2-regrefine6-mixdata-train320x576-4e7b215d.pth \
+--output_path output/gmflow-scale2-regrefine6-davis \
+--padding_factor 16 \
+--upsample_factor 4 \
+--num_scales 2 \
+--attn_splits_list 2 8 \
+--corr_radius_list -1 4 \
+--prop_radius_list -1 1 \
+--reg_refine \
+--num_reg_refine 2
